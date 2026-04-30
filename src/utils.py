fa127b772e9ac2b994ba615d71eae537d9a38ce2
bentanlw <bentlw@gmail.com>
2026-04-30 23:22:13 +0800

    moved conversion function to utils file

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+from textnode import TextNode, TextType
+from leafnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(value=text_node.text, tag=None)
+        case TextType.BOLD:
+            return LeafNode(value=text_node.text, tag="b")
+        case TextType.ITALIC:
+            return LeafNode(value=text_node.text, tag="i")
+        case TextType.CODE:
+            return LeafNode(value=text_node.text, tag="code")
+        case TextType.LINK:
+            return LeafNode(value=text_node.text, tag="a", props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(value="", tag="img", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError("invalid text type")
